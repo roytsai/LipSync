a037798d19b7ddb8895d306621a8d8ce6c331566
Gary Chiu <gary@luffydesign.com>
2025-05-23 16:17:46 +0800

    add env for dev/stage/production

---
+++ app/asr/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Asr from "@/feature/Asr";
+import React from "react";
+
+function Page() {
+  return (
+    <div className="App">
+      <Asr />
+    </div>
+  );
+}
+
+export default Page;
---
+++ feature/Asr/index.tsx
@@ -0,0 +1,9 @@
+const A2F: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="App text-[48px]">{"DS Asr"}</div>
+    </div>
+  );
+};
+
+export default A2F;
